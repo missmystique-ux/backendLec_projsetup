1f6a71fd6faddab869c2c50f227ac5b4d4a353d2
missmystique-ux <minepalwasha@gmail.com>
2026-07-19 16:44:58 +0500

    set all configured

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 
-import mongoose from "mongoose";
-import {DB_NAME} from "./constant.js";
+// import mongoose from "mongoose";
+// import {DB_NAME} from "./constant.js";
 
 import connectDB from "./db/index.js";
+import {app} from "./app.js"
 
 dotenv.config({
     path:'./.env'
